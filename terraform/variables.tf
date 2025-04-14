9855ebcb463989d2047764d2699f7c4e2ae6f339
Balu Sreeramula <balusreeramula@gmail.com>
2025-04-14 20:45:19 +0530

    Added iam_user.tf and modified variables.tf and terraform.tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,11 @@ variable "github_owner" {}
 variable "github_repo" {}
 variable "github_branch" {}
 variable "github_oauth_token" {} # Not committed, use terraform.tfvars or env var
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+}
+variable "iam_user_name" {
+  description = "Name of the IAM user for CI/CD"
+  type        = string
+}
