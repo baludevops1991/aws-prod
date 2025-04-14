f120a99608a793fe3583adfed5cedd420037201a
Balu Sreeramula <balusreeramula@gmail.com>
2025-04-14 21:05:19 +0530

    Modified codepipeline.tf

--- terraform/iam/codepipeline_role.tf
+++ terraform/iam/codepipeline_role.tf
@@ -27,12 +27,16 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
           "codecommit:Describe*",
           "s3:*",
           "iam:GetRole",
-          "iam:PassRole",   # Grant permission to pass the roles
+          "iam:PassRole",   
           "cloudwatch:PutMetricData",
           "sns:*"
         ]
-        Resource = "*"
+        Resource = [
+          var.codebuild_role_arn,
+          var.codedeploy_role_arn
+        ]
       }
     ]
   })
 }
+
